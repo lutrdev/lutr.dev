afcfcda447c76a3a8fd441adb2d3ec9cf2b5d882
Luca1152 <t.luca1152@gmail.com>
2022-03-25 18:52:28 +0200

    feat: add buttons to socials
    Now, in the header, there are two buttons - one to my LinkedIn page and one to my GitHub account.

--- src/lib/layout/Header.tsx
+++ src/lib/layout/Header.tsx
@@ -1,14 +1,44 @@
-import { Box, Flex, Heading } from "@chakra-ui/react";
-import Link from "next/link";
+import { Flex, Heading, IconButton } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { GrGithub, GrLinkedinOption } from "react-icons/gr";
+
+const LinkedInButton = () => {
+  return (
+    <NextLink href="https://www.linkedin.com/in/luca-trusca/" passHref>
+      <IconButton
+        as="a"
+        target="_blank"
+        aria-label="LinkedIn button"
+        icon={<GrLinkedinOption />}
+      />
+    </NextLink>
+  );
+};
+
+const GitHubButton = () => {
+  return (
+    <NextLink href="https://github.com/Luca1152" passHref>
+      <IconButton
+        as="a"
+        target="_blank"
+        aria-label="GitHub button"
+        icon={<GrGithub />}
+      />
+    </NextLink>
+  );
+};
 
 const Header = () => {
   return (
     <Flex as="header" width="full" align="center">
       <Heading as="h1" size="md">
-        <Link href="/">Luca Trușcă</Link>
+        <NextLink href="/">Luca Trușcă</NextLink>
       </Heading>
 
-      <Box marginLeft="auto" />
+      <Flex marginLeft="auto" gap={4}>
+        <LinkedInButton />
+        <GitHubButton />
+      </Flex>
     </Flex>
   );
 };
